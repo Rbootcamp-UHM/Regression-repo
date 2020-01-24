36e5ed452873d6a8a81506192885146473eda43b
Ethan Hill <hille7@hawaii.edu>
2020-01-24 10:56:06 -1000

    Homework 2

--- iris_regressionfunction_EH.R
+++ iris_regressionfunction_EH.R
@@ -6,8 +6,7 @@ dim(iris)	# dimensions
 names(iris) <- c("sepalL", "sepalW",  "petalL", "petalW", "species")
 species <- iris$species
 
-color <- rep("red", 150)
-color[species == "setosa"] <- "purple"
+color <- rep("purple", 150)
 color[species == "versicolor"] <- "blue"
 color[species == "virginica"] <- "orange"
 
@@ -33,6 +32,7 @@ lineplot <- function(x=iris$petalL, y=iris$sepalW, cint=int, cslope=slope, ccol=
 	abline(cint, cslope) # drawing the regression line on the plot
 	points(x, yhat, pch=7)  # adding the fitted points
 	segments(x, y, x, yhat, col="black", lty=2 )
+	legend('top', legend = levels(species), col = c("purple", "blue", "orange"), cex = .8, pch = 19)
 	text(6,4.4, paste("SSE:", round(sse, 3)))
 	text(6,4.25, paste("slope:", round(cslope, 3)))
 	text(6,4.1, paste("y-int:", round(cint, 3)))
@@ -58,4 +58,5 @@ logtransform(iris$petalW, iris$petalL)
 logtransform(iris$petalL, iris$sepalL)
 
 m <- matrix(data=c(1:100), ncol=10, nrow=10)
-logtransform(m[,1], m[,7])
+logtransform(m[,1], m[,2])
+logtransform(m[,1], m[,10])
