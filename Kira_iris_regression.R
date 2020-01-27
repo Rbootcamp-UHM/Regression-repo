af328fc4d17dbe65faf3e2165edb3cabe1b50721
Kira Webster <kiramw@hawaii.edu>
2020-01-26 17:30:50 -1000

    Homework 2 corrections

--- Kira_iris_regression.R
+++ Kira_iris_regression.R
@@ -1,54 +1,41 @@
-iris	# Fisherʻs iris dataset
-dim(iris)	# dimensions
-names(iris)
+iris	#making sure Iris dataset is active
+names(iris)	#checking Iris names so you can rename them
+names(iris) <- c("sepalL", "sepalW",  "petalL", "petalW", "species")	#Renaming iris with more convenient names
 
-## Renaming iris with more convenient names
-## Accessing vectors within iris, saving as separate objects
-
-names(iris) <- c("sepalL", "sepalW",  "petalL", "petalW", "species")
-species <- iris$species   
+species <- iris$species   #saving specific values from the Iris dataset as variables to use later
 x <- iris$petalL
 y <- iris$petalW
 
+fit <- lm( y ~ x )  	#creating variable fit for the linear model
+b <- coef( fit )	 #naming b as the coefficient for the model
+int <- b[1] 	#redelegating b as the intercept, such as in y=mx+b
+slope <- b[2] 	#naming slope for the linear model
 
-## Illustrating regression by graphing 
+plot(x, y) 		#now we can plot our previously named x as petal length and y as petal width 
+abline(fit) 	#abline creates a line of best fit on the plot given the slope and intercept already laid out
 
-fit <- lm( y ~ x )  # fitting the linear model
-b <- coef( fit ) # grabbing the coefficients
-int <- b[1] # intercept
-slope <- b[2] # slope
+yhat <- slope*x + int	 #labeling the linear formula y=mx+b as yhat because y is already taken
 
-plot(x, y) # plotting the data points
-abline(fit) # drawing the regression line on the plot
-# look up help for ?abline and figure out how to use 
-# abline by specify slope and intercept.
+points( x, yhat, pch=3)  #making the points that fall on the line of best fit obvious
 
-yhat <- slope*x + int # the fitted values
+i=150		#we now want to show the error margin between the points on contrasting sides of the line of best fit
+segments( x[i], y[i], x[i], yhat[i], col="red" ) 	#we name segments as the connection between these points
+segments( x, y, x, yhat, col="red", lty=2 )		#we formulate the segments and now they will appear
 
-points( x, yhat, pch=3)  # adding the fitted points
+error <- (yhat-y)		#to calculate error between the points
+sse <- sum(error^2)		#to calculate the sum of squared errors
 
-## if we want to draw line segments between the 150th
-## point and the fitted regression value for it:
-i=150
-segments( x[i], y[i], x[i], yhat[i], col="red" )
-
-## R is "vectorized": it will work on vectors all at once:
-segments( x, y, x, yhat, col="red", lty=2 )
-
-sse <- (yhat-y)
-SSE <- sum((sse))^2
-
-text(2,2.5, paste("Slope:", slope))
-text(2,2.25, paste("y-int:", int))
-text(2,2, paste("SSE:", SSE))
+text(2,2.5, paste("Slope:", slope))		#to add the text for slope to the graph
+text(2,2.25, paste("y-int:", int))		#to add the text for y-intercept to the graph
+text(2,2, paste("SSE:", sse))			#to add the text for SSE to the graph
 
 
 
 #Function Wrap
 myplot <- function(x=iris$petalL, y=iris$petalW, myint=int, myslope=slope)	{
 	yhat <- myslope*x + myint
-	se <- yhat - y
-	sse <- sum(se)^2
+	error <- yhat - y
+	sse <- sum(error^2)
 	plot(x, y)
 	abline(myint, myslope)
 	points(x, yhat, pch=3)
@@ -67,8 +54,14 @@ myplot(myint=0, myslope=1)
 
 
 #My Function
-carsfunction <- function(x=cars$speed, y=cars$dist)	{
-	x <- x*1.60934
-	y <- y*1.60934
+conversionfunction <- function(x=cars$speed, y=cars$dist, conv1=1.60934, conv2=0.3048)	{
+	x <- x*conv1
+	y <- y*conv2
 	plot(x, y)
+	fit <- lm (y~x)
+	abline(fit)
 }
+
+conversionfunction()		#testing function with cars data, this converts speed from miles to kilometers, and distance from feet to meters. 
+conversionfunction(x=women$height, y=women$weight, conv1=2.54, conv2=0.4536)	 #using the dataset women, this converts height from inches to centimeters, and weight from pounds to kilograms
+conversionfunction(x=trees$Girth, y=trees$Height, conv1=2.54, conv2=0.3048)		#using the dataset trees, this converts tree girth from inches to centimeters, and tree height from feet to meters. 
